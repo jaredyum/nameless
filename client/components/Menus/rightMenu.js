aced364144932f8249e14a092fb46083972b5d0a
Daniel Lochrie <dclochri@gmail.com>
2018-07-20 10:12:43 -0500

    Fixed issues where React was failing to load components.

--- client/components/Menus/rightMenu.js
+++ client/components/Menus/rightMenu.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 // MUI Components
 import IconButton from '@material-ui/core/IconButton';
-import Menu, { MenuItem } from '@material-ui/core/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 // Actions
