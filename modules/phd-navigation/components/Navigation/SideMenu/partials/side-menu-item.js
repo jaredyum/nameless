78c10dfcae1507d38a354b64c656b9c5d1c3c8c3
Daniel Lochrie <dclochri@gmail.com>
2018-09-10 16:11:47 -0500

    Fixed an issue where active links were not rendering properly.

--- modules/phd-navigation/components/Navigation/SideMenu/partials/side-menu-item.js
+++ modules/phd-navigation/components/Navigation/SideMenu/partials/side-menu-item.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // React Router
 import { NavLink } from 'react-router-dom';
@@ -10,45 +10,69 @@ import ListItem from '@material-ui/core/ListItem';
 // Local styles
 import styles from '../../styles';
 
-const SideMenuItem = (props) => {
-  const {
-    classes,
-    label,
-    path,
-    clickHandler,
-    toggleHandler
-  } = props;
+class SideMenuItem extends Component {
+  constructor(props) {
+    super(props);
 
-  // If this is an action item (NOT a link), then clicking should BOTH execute
-  // the action AND close the menu.
-  if (clickHandler) {
-    const closeAndExec = () => {
-      toggleHandler();
-      clickHandler();
-    };
+    this.isActive = this.isActive.bind(this);
+  }
+
+  /**
+   * Determines if the link is active or not.
+   * @param {!Object} match The react router match object.
+   * @param {!Object} location The react router location object.
+   * @param {string} location.pathname The pathname (as reported by react
+   *   router.)
+   * @return {boolean} Whether or not the link item is active.
+   */
+  isActive(match, location) {
+    const { path } = this.props;
+    const { pathname } = location;
+
+    return path === pathname;
+  }
+
+  render() {
+    const {
+      classes,
+      label,
+      path,
+      clickHandler,
+      toggleHandler
+    } = this.props;
+
+    // If this is an action item (NOT a link), then clicking should BOTH execute
+    // the action AND close the menu.
+    if (clickHandler) {
+      const closeAndExec = () => {
+        toggleHandler();
+        clickHandler();
+      };
+
+      return (
+        <ListItem title={label} onClick={closeAndExec} key={label}>
+          <span className={classes.link}>{ label }</span>
+        </ListItem>
+      );
+    }
 
     return (
-      <ListItem title={label} onClick={closeAndExec} key={label}>
-        <span className={classes.link}>{ label }</span>
+      <ListItem title={label} key={label}>
+        <NavLink
+          to={path}
+          className={classes.link}
+          onClick={toggleHandler}
+          replace
+          activeClassName={classes.active}
+          isActive={this.isActive}
+          exact
+        >
+          { label }
+        </NavLink>
       </ListItem>
     );
   }
-
-  return (
-    <ListItem title={label} key={label}>
-      <NavLink
-        to={path}
-        className={classes.link}
-        onClick={toggleHandler}
-        replace
-        exact
-        activeClassName={classes.active}
-      >
-        { label }
-      </NavLink>
-    </ListItem>
-  );
-};
+}
 
 export const SideMenuItemJest = SideMenuItem;
 
